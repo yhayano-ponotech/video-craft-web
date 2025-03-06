e2c4488f392ef0fba08c61713320e38f0260e80b
yhayano-ponotech <yhayano.biz@gmail.com>
2025-03-06 12:30:07 +0900

    nextjsの設定ファイルを追加

---
+++ next.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['i.ytimg.com', 'img.youtube.com'],
+    },
+    // バックエンドAPIへのプロキシ設定
+    async rewrites() {
+        // Check if BACKEND_URL is defined, otherwise use a default value or return empty array
+        if (process.env.BACKEND_URL) {
+            return [
+            {
+                source: '/api/:path*',
+                destination: process.env.BACKEND_URL + '/api/:path*',
+            },
+            ];
+        }
+        // If BACKEND_URL is not defined, return empty array to avoid the error
+        return [];
+    },
+    // ESLintの自動設定を無効化
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
+
+export default nextConfig;
